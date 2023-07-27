5e01fea39c7290e855734bffaa7fe22781b4a924
caglaaksoy <caglaaksoy2001@gmail.com>
2023-07-27 11:51:05 +0300

    Add project files.

---
+++ WebProjectDesign/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProjectDesign.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            ///kullanıcı adı "admin", şifre "1234"
+            if (username == "admin" && password == "1234")
+            {
+                ViewBag.Mesaj = "Giriş başarılı.";
+                return RedirectToAction("Index", "Proje");
+            }
+            else
+            {
+                ViewBag.Mesaj = "Giriş başarısız.";
+            }
+            return View();
+        }
+
+    }
+}
---
+++ WebProjectDesign/Controllers/ProjeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProjectDesign.Controllers
+{
+    public class ProjeController : Controller
+    {
+        // GET: Proje
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
